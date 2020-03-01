069d2eb2fda36568b19b5d90dd60d0eec1095a78
anbreaker <antunez19@gmail.com>
2020-03-01 10:11:21 +0100

    agregando templates dinamicos incio

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hola():
-    return '¡Hola, mundo con flask!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
