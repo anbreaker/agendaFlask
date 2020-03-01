af753e7089190010a8e94d8f4444304ad1fe8c10
anbreaker <antunez19@gmail.com>
2020-03-01 11:22:49 +0100

    probando flask

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route(r'/', methods=['GET'])
 def hola():
-    return render_template('index.html')
+    return render_template('agenda.html')
+
+
+@app.route(r'/agregar', methods=['GET', 'POST'])
+def agregar_contacto():
+    
+    if request.form:
+        print(request.form.get('nombre'))
+        print(request.form.get('movil'))
+        print(request.form.get('mail'))
+    
+    return render_template('agregar_contacto.html')
+
 
 
 if __name__ == '__main__':
